060e948f17895a3ac66627bd1678e8ae9c049195
Airydev001 <josysmith02@gmail.com>
2023-07-13 10:23:05 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Stack(children: [
+    Container(
+      width: 250,
+      height: 250,
+      color: Colors.amber,
+    )
+  ]))));
+}
